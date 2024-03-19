bafb6778eea6ef60e4cdf4c93e6308f1ce4d7aae
Jimin Choi <jimchoi@c2r2s8.42seoul.kr>
2024-03-19 20:39:18 +0900

    0319

--- bresenham.c
+++ bresenham.c
@@ -8,5 +8,74 @@ void bresenham(int x1, int y1, int x2, int y2)
 	int dx = x2 - x1;
 	int dy = y2 - y1;
 
-	char szstr[64];
+
+	//음수방향
+	if (dx < 0)
+    {
+        dx = -dx;
+        addx = -1;
+    }
+    else
+    {
+        addx = 1;
+    }
+	if (dy < 0)
+	{
+		dy = -dy;
+        addy = -1;
+	}
+	else{
+		addy = 1;
+	}
+
+	int x = x1;
+	int y = y1;
+
+	if (dx >= dy)
+    {
+		for(int i = 0; i < dx; i++)
+		{
+			//x를 1증가 시키고 dy만큼 count 증가
+			x += addx;
+			//x의 증가만큼 count 증가
+			count += dy;
+
+			//증가하는 y값보다 크다면 
+			if(count >= dx)
+			{
+				//y 를 1 증가시키고 count를 초기화
+				y += addy;
+                count -= dx;
+			}
+			printf("x = %d, y = %d\n", x, y);
+
+		}
+    }
+	//dx < dy 일 경우 (기울기 > 1) x의 조건이 만족하면 y를 1 증가시킨다
+    else
+    {
+        for(int i=0; i<dy; i++)
+		{
+			//y를 1증가 시키고 dx만큼 count 증가
+            y += addy;
+            //y의 증가만큼 count 증가
+            count += dx;
+
+            //증가하는 x값보다 크다면 
+            if(count >= dy)
+            {
+                //x 를 1 증가시키고 count를 초기화
+                x += addx;
+                count -= dy;
+            }
+            printf("x = %d, y = %d\n", x,y);
+		}
+    }
+}
+
+
+
+int main()
+{
+	bresenham(0,0,10,20);
 }
--- test.c
+++ test.c
@@ -27,6 +27,7 @@ void			my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 void	parsing();
+void bresenham(int x1, int y1, int x2, int y2, t_data image);
 int main()
 {
 	void	*mlx_ptr;
@@ -35,15 +36,16 @@ int main()
 
 	printf("Loading\n");
 
-	parsing();
+	// parsing();
 
-	// mlx_ptr = mlx_init();
-	// win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Hellow World!");
-	// image.img = mlx_new_image(mlx_ptr, 500, 500); // 이미지 객체 생성
-	// image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian); // 이미지 주소 할당
-	// 		double x = 0;
-	// 		double y = 0;
-	// 		double theta = 3.14 / 4.0;
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Hellow World!");
+	image.img = mlx_new_image(mlx_ptr, 500, 500); // 이미지 객체 생성
+	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian); // 이미지 주소 할당
+			double x = 0;
+			double y = 0;
+			double theta = 3.14 / 4.0;
+	bresenham(0 , 0, 100, 200, image);
 	// for (int i = 0 ; i <= 200 ; i++)
 	// {
 			
@@ -57,11 +59,11 @@ int main()
 	// 		}
 	// 	}	
 	// }
-	// mlx_put_image_to_window(mlx_ptr, win_ptr, image.img, 0, 0);
-	// mlx_loop(mlx_ptr);
-	// return (0);
+	mlx_put_image_to_window(mlx_ptr, win_ptr, image.img, 0, 0);
+	mlx_loop(mlx_ptr);
+	return (0);
 }
-void check_leaks(void) 
+void check_leaks(void)
 { system("leaks test1"); } 
 void	parsing()
 {
@@ -102,4 +104,83 @@ void	parsing()
 	// free(str);
 	// }
 	return;
+}
+
+void bresenham(int x1, int y1, int x2, int y2, t_data image)
+{
+	int addx, addy;
+	int count = 0;
+
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+
+
+	//음수방향
+	if (dx < 0)
+    {
+        dx = -dx;
+        addx = -1;
+    }
+    else
+    {
+        addx = 1;
+    }
+	if (dy < 0)
+	{
+		dy = -dy;
+        addy = -1;
+	}
+	else{
+		addy = 1;
+	}
+
+	int x = x1;
+	int y = y1;
+	// printf("x: %d, y: %d\n", x, y);
+	if (dx >= dy)
+    {
+		printf("xxxx");
+		for(int i = 0; i < dx; i++)
+		{
+			//x를 1증가 시키고 dy만큼 count 증가
+			x += addx;
+			//x의 증가만큼 count 증가
+			count += dy;
+
+			//증가하는 y값보다 크다면 
+			if(count >= dx)
+			{
+				//y 를 1 증가시키고 count를 초기화
+				y += addy;
+                count -= dx;
+			}
+			printf("x = %d, y = %d\n", x, y);
+			my_mlx_pixel_put(&image,x, y ,  0x00FF00 );
+
+
+		}
+    }
+	//dx < dy 일 경우 (기울기 > 1) x의 조건이 만족하면 y를 1 증가시킨다
+    else
+    {
+		printf("yyyy");
+        for(int i=0; i<dy; i++)
+		{
+			
+			//y를 1증가 시키고 dx만큼 count 증가
+            y += addy;
+            //y의 증가만큼 count 증가
+            count += dx;
+
+            //증가하는 x값보다 크다면 
+            if(count >= dy)
+            {
+                //x 를 1 증가시키고 count를 초기화
+                x += addx;
+                count -= dy;
+            }
+            printf("x = %d, y = %d\n", x,y);
+			my_mlx_pixel_put(&image,x, y ,  0x00FF00 );
+		}
+    }
 }
